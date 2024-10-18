6bdba375efbe18bb9ae358b302b8f4d0135a8501
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-10-18 22:23:06 +0300

    mqtt listen and send feature is added

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const mqtt = require("mqtt");
+const client = mqtt.connect("tcp://broker.mqtt.cool:1883");
+const topics = ["mustafa"];
 
-console.log(mqtt);
+client.on("connect", () => {
+  console.log("[+] Client connected");
+
+  client.subscribe(topics, () => {
+    console.log(`Subscribing this topics: ${topics}`);
+    setTimeout(() => {
+      client.publish(topics[0], "naber");
+    }, 3000);
+  });
+});
+
+client.on("message", (topic, message) => {
+  console.log({ topic, message, str: message.toString() });
+});
